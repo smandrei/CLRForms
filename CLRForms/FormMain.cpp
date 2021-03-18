92245e83d3922697d36ef7fe6d685e8036a8b567
smandrei <s.andrei22052017@gmail.com>
2021-03-18 19:46:22 +0300

    Dobavlena glavnaya forma i funkciya void Main(array <String^>^ args)

---
+++ CLRForms/FormMain.cpp
@@ -0,0 +1,12 @@
+#include "FormMain.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void Main(array <String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	CLRForms::FormMain form;
+	Application::Run(% form);
+}
